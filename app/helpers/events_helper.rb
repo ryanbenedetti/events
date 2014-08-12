19045dae50477d06aea9c5d6f499d3b2440bff80
Ryan Benedetti <rhino.j.benedetti@gmail.com>
2014-08-12 10:16:46 -0700

    Helpers lesson complete.

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,11 @@
 module EventsHelper
+  def format_price(event)
+    if event.free?
+       #option 1: "<strong>Free</strong>".html_safe
+       #option 2 (better): 
+       content_tag(:strong, "Free!")
+      else
+        number_to_currency(event.price)
+    end
+  end
 end
