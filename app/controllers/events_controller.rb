8d684429fb82efd3ac10ad7a19318a7f8ad507cc
Ryan Benedetti <rhino.j.benedetti@gmail.com>
2014-08-12 16:51:30 -0700

    Routes -> show page complete.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,4 +3,9 @@ class EventsController < ApplicationController
     #First iteration : @events = ['Bug Smash', 'Hackathon', 'Kata Camp', 'Rails User Group']
     @events = Event.all
   end
+  
+  def show
+    #find details for each record
+    @event = Event.find(params[:id])
+  end
 end
